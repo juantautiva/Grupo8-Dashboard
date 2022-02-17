42002f907c5ccb9197b75d76928b1905c4b54e24
Joaquin Cernik <joaquincernik@gmail.com>
2022-02-17 11:19:55 -0300

    Subida de categorias

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,71 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import CategoriesProps from "./CategoriesProps";
 
-function GenresInDb() {
+
+function Categories() {
+
+  const [ categories, setCategories] = useState([])
+  
+  useEffect(() => {
+  fetch('/api/categories')
+    .then(response => response.json())
+    .then(data => setCategories(data.countByCategory))
+          .catch(error => console.log(error))
+}, [])
+
   return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
+   
+    <div className="col-lg-6 mb-4">						
+    <div className="card shadow mb-4">
         <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            Genres in Data Base
-          </h5>
+            <h6 className="m-0 font-weight-bold text-gray-800">Categorias</h6>
         </div>
         <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
+            <div className="row">
+                {
+                    categories.map((category,index)=>{
+                        
+                        return  <CategoriesProps  name={category.categoryName} count={category.count}  key={index} />
+                    })
+                }
             </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  );
+</div>
+        );
 }
 
-export default GenresInDb;
+export default Categories;
---
+++ src/components/categories/CategoriesProps.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import Categories from "./Categories";
+
+
+function CategoriesProps(props) {
+
+    return (
+        <div className="col-lg-6 mb-4">
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h5 className="m-0 font-weight-bold text-gray-800">
+                {props.name} 
+            </h5>
+          </div>
+            
+          <div className="card-body">
+            <div className="row">
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-dark text-white shadow">
+                  <div className="card-body">
+                    {props.count}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+       
+         
+      </div>
+      </div>
+      
+    )
+            }
+
+export default CategoriesProps
